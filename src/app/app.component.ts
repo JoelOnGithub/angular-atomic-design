6e05bf28237fd9ad0fec68c11259a5f1f13033a9
Joel Scholten <jscholten@yellowtail.nl>
2020-07-07 12:01:32 +0200

    Added inputs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'atomic-inputs';
+  form: FormGroup;
+
+  constructor(fb: FormBuilder) {
+    this.form = fb.group({
+      field1: ['value1', [Validators.required]],
+      field2: ['value2', [Validators.required]],
+    });
+  }
+
+  onSubmit() {
+    console.log(this.form.value);
+  }
+
 }
